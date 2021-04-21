822192e41df72f94686bcc22b82358a6d40b2625
pereser <rodrigopr404@gmail.com>
2021-04-21 20:09:52 -0300

    Terceiro dia

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,6 +1,5 @@
 import { Request, Response} from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "../repositories/SettingRepository";
+import { SettingsService } from "../services/SettingsService";
 
 
 
@@ -8,16 +7,19 @@ class SettingsController {
 
     async create(request: Request, response: Response){
         const {chat, username} = request.body;
-        const settingsRepository = getCustomRepository(SettingsRepository);
-    
-        const settings = settingsRepository.create({
-            chat,
-            username
-        })
-    
-        await settingsRepository.save(settings);
-    
+
+        const settingsService = new SettingsService();
+    try{
+        const settings = await settingsService.create({chat, username});
+
         return response.json(settings)
+
+    }catch(err){
+        return response.status(400).json({
+            message: err.message
+        })
+
+    }
 
     }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
+import { MessagesController } from "./controllers/MessagesController";
 import { SettingsController } from "./controllers/SettingsController";
+import { UsersController } from "./controllers/UsersController";
 
 
 const routes = Router();
+
 const settingsControlController = new SettingsController();
+const usersController = new UsersController();
+const messagesController = new MessagesController();
 
 routes.post("/settings", settingsControlController.create)
+routes.post("/users", usersController.create)
+routes.post("/messages", messagesController.create)
+routes.get("/messages/:id", messagesController.showByUser)
 
 export{routes};
